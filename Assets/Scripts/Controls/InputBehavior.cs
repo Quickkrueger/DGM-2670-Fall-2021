f195d1335e139ac8b65d6276a3e155822ba16350
Marshall <10923227@uvu.edu>
2021-09-15 11:38:20 -0600

    Testing out inputsystem

--- Assets/Scripts/Controls/InputBehavior.cs
+++ Assets/Scripts/Controls/InputBehavior.cs
@@ -1,24 +1,55 @@
+
+using Packages.Rider.Editor.UnitTesting;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class InputBehavior : MonoBehaviour
 {
+    public ControlsData controlsData;
 
-    public UnityEvent mouseDown;
-    public UnityEvent mouseUp;
-    public UnityEvent<KeyCode> keyDown;
-    public UnityEvent<KeyCode> keyPressed;
-    public UnityEvent<KeyCode> keyUp;
+    private InputAction jump;
+    private InputAction attack;
+    private InputAction right;
+    private InputAction left;
 
-    private void OnMouseDown()
+    private void Start()
     {
-        mouseDown.Invoke();
+        jump = new InputAction();
+        attack = new InputAction();
+        right = new InputAction();
+        left = new InputAction();
+        
+        jump = controlsData.jump.action;
+        attack = controlsData.attack.action;
+        right = controlsData.right.action;
+        left = controlsData.left.action;
+
+        jump.performed += Jump;
+        attack.performed += Attack;
+        right.performed += Right;
+        left.performed += Left;
     }
 
-    private void OnMouseUp()
+    private void Left(InputAction.CallbackContext obj)
     {
-        mouseUp.Invoke();
+        
+    }
+
+    private void Right(InputAction.CallbackContext obj)
+    {
+        
+    }
+
+    private void Attack(InputAction.CallbackContext obj)
+    {
+        
+    }
+
+    private void Jump(InputAction.CallbackContext obj)
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * 1000f);
     }
     
     
--- Assets/Scripts/ScriptableObjects/ControlsData.cs
+++ Assets/Scripts/ScriptableObjects/ControlsData.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 [CreateAssetMenu(fileName = "ControlsData", menuName = "ScriptableObjects/Utility/ControlsData", order = 1)]
 public class ControlsData : ScriptableObject
@@ -9,33 +10,13 @@ public class ControlsData : ScriptableObject
     public ControlPairing attack;
 
     public bool UseTouch;
-
-    public float interactionMinimum;
-    private Vector2 interactionOffset;
-
-    public void StartTouch(Vector2 initial)
-    {
-        interactionOffset = initial;
-    }
-
-    public ControlPairing EndTouch(Vector2 final)
-    {
-        interactionOffset -= final;
-
-        if (interactionMinimum > interactionOffset.magnitude)
-        {
-            return attack;
-        }
-
-        return jump;
-    }
     
     
 [System.Serializable]
     public struct ControlPairing
     {
         public string controlName;
-        public KeyCode controlKey;
+        public InputAction action;
     }
 
 }
