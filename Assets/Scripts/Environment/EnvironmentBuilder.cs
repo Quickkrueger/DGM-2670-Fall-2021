3544a77cb74bec6f6d8194be016ad09475008148
Marshall <10923227@uvu.edu>
2021-10-04 11:31:18 -0600

    Started initial platform generation

--- Assets/Scripts/Environment/EnvironmentBuilder.cs
+++ Assets/Scripts/Environment/EnvironmentBuilder.cs
@@ -1,5 +1,7 @@
 using System.Collections;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnvironmentBuilder : MonoBehaviour
 {
@@ -13,6 +15,8 @@ public class EnvironmentBuilder : MonoBehaviour
     public GameObject tilePrefab;
     public GameObject newestTile;
 
+    public float buildSpeed = 0.1f;
+
     private Coroutine builderRoutine;
 
     private void Awake()
@@ -24,12 +28,23 @@ public class EnvironmentBuilder : MonoBehaviour
     {
         int rand = Random.Range(0, 2);
 
-        int numTiles = Random.Range(5, 10);
+        int numTiles;
 
-        WaitForSeconds wait = new WaitForSeconds((float) numTiles * 0.1f - Time.deltaTime / 2);
-        
-        StartCoroutine(BuildGround(wait, numTiles));
-            
+        WaitForSeconds wait;
+
+        if (rand == 0)
+        {
+            numTiles = Random.Range(5, 10);
+            wait = new WaitForSeconds((float) numTiles * buildSpeed - Time.deltaTime / 2);
+            StartCoroutine(BuildGround(wait, numTiles));
+        }
+        else if (rand == 1)
+        {
+            numTiles = Random.Range(3, 6);
+            wait = new WaitForSeconds((float) numTiles * buildSpeed - Time.deltaTime / 2);
+            StartCoroutine(BuildPlatforms(wait, numTiles));
+        }
+
         yield return new WaitForSeconds(0f);
         builderRoutine = null;
     }
@@ -37,6 +52,7 @@ public class EnvironmentBuilder : MonoBehaviour
     IEnumerator BuildGround(WaitForSeconds wait, int numTiles)
     {
         int randTile;
+        int randDist;
         for (int i = 0; i < numTiles; i++){
 
             randTile = Random.Range(0, enviromentData.groundTiles.Length);
@@ -44,6 +60,11 @@ public class EnvironmentBuilder : MonoBehaviour
             if (newestTile == null)
             {
                 newestTile = CreateTile(randTile, 0f, enviromentData.groundTiles, ground.position);
+            }
+            else if (i == 0)
+            {
+                randDist = Random.Range(5, 8);
+                newestTile = CreateTile(randTile, 0f, enviromentData.groundTiles, new Vector3(newestTile.transform.position.x, ground.position.y, 0f)  + Vector3.right * (float)randDist);
             }
             else
             {
@@ -74,16 +95,37 @@ public class EnvironmentBuilder : MonoBehaviour
 
     GameObject CreateTile(int randTile, float positionCount, TileData[] tileSet, Vector3 referencePosition)
     {
-        GameObject newTile = Instantiate(tilePrefab, referencePosition + Vector3.down * positionCount, ground.rotation);
+        GameObject newTile = Instantiate(tilePrefab, referencePosition + Vector3.down * positionCount, Quaternion.identity);
         newTile.GetComponent<EnvironmentTile>().UpdateTile(tileSet[randTile]);
         
         return newTile;
     }
 
-    IEnumerator BuildPlatforms(WaitForSeconds wait)
+    IEnumerator BuildPlatforms(WaitForSeconds wait, int numTiles)
     {
-        
-        
+        int randTile;
+        int randDist;
+        for (int i = 0; i < numTiles; i++){
+
+            randTile = Random.Range(0, enviromentData.groundTiles.Length);
+            
+            if (newestTile == null)
+            {
+                newestTile = CreateTile(randTile, 0f, enviromentData.groundTiles, platform.position);
+            }
+            else if (i == 0)
+            {
+                randDist = Random.Range(1, 8);
+                newestTile = CreateTile(randTile, 0f, enviromentData.groundTiles, newestTile.transform.position + Vector3.up * 2f  + Vector3.right * (float)randDist);
+            }
+            else
+            {
+                newestTile = CreateTile(randTile, 0f, enviromentData.groundTiles, newestTile.transform.position + Vector3.right);
+            }
+
+
+        }
+
         yield return wait;
         if (builderRoutine == null)
         {
